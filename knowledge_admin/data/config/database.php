e07b944fd7a6fe2333f4c3b668bc620b14e50094
万岳科技 <8162876+WanYueKeJi_343188225@user.noreply.gitee.com>
2022-12-26 02:13:57 +0000

    更新

--- knowledge_admin/data/config/database.php
+++ knowledge_admin/data/config/database.php
@@ -20,11 +20,11 @@ return [
     // 服务器地址
     'hostname' => '127.0.0.1',
     // 数据库名
-    'database' => 'knowledge',
+    'database' => '',
     // 用户名
-    'username' => 'root',
+    'username' => '',
     // 密码
-    'password' => 'root',
+    'password' => '',
     // 端口
     'hostport' => '3306',
     // 数据库编码默认采用utf8
@@ -36,6 +36,6 @@ return [
 	
 	/* redis */
 	'REDIS_HOST' => "127.0.0.1",
-	'REDIS_AUTH' => "123456",
+	'REDIS_AUTH' => "",
 	'REDIS_PORT' => "6379",
 ];
